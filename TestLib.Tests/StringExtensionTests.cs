13ce4202ace847153e65d18aec7514fcc81a0002
Gaute Løken <gautelo@gmail.com>
2019-03-03 00:11:13 +0100

    Added a unit test project with a test for StringExtensions.SplitList()

---
+++ TestLib.Tests/StringExtensionTests.cs
@@ -0,0 +1,16 @@
+using Xunit;
+
+namespace GauteLo.TestLib
+{
+    public class StringExtensionTests
+    {
+        [Fact]
+        public void SplitList_Works()
+        {
+            string[] expected = { "some", "other", "thing" };
+            var actual = "some;other;;thing".SplitList();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
